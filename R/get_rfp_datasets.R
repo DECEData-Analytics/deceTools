c7953d06821fd3577c1b9bdcd5cbb89b93fba246
Cory Pilat <56729741+cpilat97@users.noreply.github.com>
2022-02-28 10:52:29 -0500

    update to the datasets generated

--- R/get_rfp_datasets.R
+++ R/get_rfp_datasets.R
@@ -16,13 +16,21 @@ get_rfp_datasets = function() {
     interview_scores <<- googlesheets4::read_sheet(ss_ems_tool, sheet = "SiteVisitScores")
 
     strength_comments <<- googlesheets4::read_sheet(ss_ems_tool, sheet = 'r_1395_96_lic_strength') %>%
-      dplyr::mutate_at('statement_of_strength', ~ stringr::str_remove(., "\\d+\\.\\s"))
+      dplyr::mutate_at('statement_of_strength', ~ stringr::str_remove(., "\\d+\\.\\s")) %>%
+      dplyr::mutate_at('statement_of_strength', ~ stringr::str_replace_na(.))
 
     growth_comments <<- googlesheets4::read_sheet(ss_ems_tool, sheet = 'r1395_96_lic_growth') %>%
-      dplyr::mutate_at('statement_of_growth', ~ stringr::str_remove(., "\\d+\\.\\s"))
+      dplyr::mutate_at('statement_of_growth', ~ stringr::str_remove(., "\\d+\\.\\s")) %>%
+      dplyr::mutate_at('statement_of_growth', ~ stringr::str_replace_na(.))
 
-    users <<- googlesheets4::read_sheet(ss_ems_tool, sheet = "Users")
+    users <<- googlesheets4::read_sheet(ss_ems_tool, sheet = "Users") %>%
+      dplyr::mutate(Team = as.character(Team),
+                    Team = stringr::str_replace_all(Team, "NULL", NA_character_)) %>%
+      tidyr::separate_rows(., Team) %>%
+      dplyr::mutate_at("Team", as.double)
 
     proposal_info <<- googlesheets4::read_sheet(ss_ems_tool, sheet = "Proposal_Info")
+
+    coversheet <<- read_sheet(ss_ems_tool, sheet = "CoverSheet")
   }
 }
